018b6238cb8a3804d4565c50780042829b4460b7
fgsfds <4870330+fgsfds@users.noreply.github.com>
2019-09-13 21:22:45 +0500

    added support for Fresh Supply

---
+++ fs_install.bat
@@ -0,0 +1,10 @@
+7za d BloodEX.kpf voxels/
+7za d BloodEX.kpf defs/extraVoxels.txt
+7za d BloodEX.kpf defs/levelHacks/
+
+7za a BloodEX.kpf defs\
+7za a BloodEX.kpf KVX\
+
+rmdir /s /q KVX
+rmdir /s /q defs
+del 7za.exe
